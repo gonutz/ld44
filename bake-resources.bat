7d78c5ef843231fc2b008b2a3687ca77ce30c344
gonutz <lars.scheme@gmail.com>
2019-04-27 08:25:00 +0200

    Add main icon

--- bake-resources.bat
+++ bake-resources.bat
@@ -2,6 +2,13 @@ go get github.com/gonutz/ico
 if errorlevel 1 (pause & exit)
 go get github.com/gonutz/bin2go/v2/bin2go
 if errorlevel 1 (pause & exit)
+go get github.com/gonutz/rsrc
+if errorlevel 1 (pause & exit)
+
+ico main-icon.png main-icon.ico
+if errorlevel 1 (pause & exit)
+rsrc -ico main-icon.ico -arch=386 -o=rsrc_386.syso
+if errorlevel 1 (pause & exit)
 
 ico decrypt-icon.png decrypt-icon.ico
 if errorlevel 1 (pause & exit)
